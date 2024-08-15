65a26079f8bc86f0c5f1b85e9451f88d8bf336b5
Aninha <clarapaulap@gmail.com>
2024-08-15 16:45:42 -0300

    php project

---
+++ HelpDesk/Admin/valida_login.php
@@ -0,0 +1,35 @@
+<?php
+// echo '!Teste Ok!';
+// print_r($_GET);
+// echo '<br>';
+// echo $_GET['Email'];
+// echo '<br>';
+// echo $_GET['Senha'];
+
+// print_r($_POST);
+// echo '<br>';
+// echo $_POST['Email'];
+// echo '<br>';
+// echo $_POST['Senha'];
+
+$usuarios_autenticado = false;
+
+$usuarios_app = array(
+    array('Email' => 'adm@teste.com.br', 'Senha' => '1234'),
+    array('Email' => 'user@teste.com.br', 'Senha' => '5678')
+);
+
+foreach($usuarios_app as $user){
+    if($user['Email'] == $_POST['Email'] && $user['Senha'] == $_POST['Senha']){
+        $usuarios_autenticado = true;
+    };
+};
+
+if($usuarios_autenticado){
+    echo 'Usuário autenticado com sucesso';
+} else {
+    // echo 'Erro de autenticação';    
+    header('Location: index.php?login=error');
+};
+
+?>
